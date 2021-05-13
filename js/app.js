c141a0d2c6b8067876a2c39406c358d5fd99cfb1
jamiguel23 <josematthewmiguel@gmail.com>
2021-05-12 18:53:29 -0700

    created seattle object and logged POL in console

--- js/app.js
+++ js/app.js
@@ -5,5 +5,15 @@
 console.log('hello');
 
 // created operation hours variable
-let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm',];
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 console.log(hours);
+
+let seattle = {
+  name: 'Seattle',
+  min: 23,
+  max: 65,
+  dailyTotal: 0,
+  avg: 6.3
+};
+
+console.log(seattle);
