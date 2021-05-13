dd2dae62f9d64fe5f60cd078e957aeee0844f24c
jamiguel23 <josematthewmiguel@gmail.com>
2021-05-12 18:22:36 -0700

    proof of life for hour variaiable

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
 
-// proof of life
+// proof of life, rn my POL does not work in the live server extension but works when it is deployed ti GitHub
 console.log('hello');
 
-
-let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm'];
-
+// created operation hours variable
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm',];
+console.log(hours);
