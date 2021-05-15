150dd813282a5902702de36c2eda910d7b7fa5b2
jamiguel23 <josematthewmiguel@gmail.com>
2021-05-15 14:07:22 -0700

    added table to sales.html, added basic styling in CSS, added commecnted out notes of expected behavior

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ console.log('hello');
 
 // created operation hours variable
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-console.log(hours);
+// console.log(hours);
 
 const seattleList = document.getElementById('seattle-list');
 const tokyoList = document.getElementById('tokyo-list');
